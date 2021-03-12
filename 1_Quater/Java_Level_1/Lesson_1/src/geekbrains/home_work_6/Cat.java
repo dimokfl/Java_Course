429e0195bd7b219cb40eed9201bb78691a2da494
dimokfl <dimokfl@mail.ru>
2021-03-12 22:12:35 +0400

    add Const

--- 1_Quater/Java_Level_1/Lesson_1/src/geekbrains/home_work_6/Cat.java
+++ 1_Quater/Java_Level_1/Lesson_1/src/geekbrains/home_work_6/Cat.java
@@ -3,6 +3,7 @@ package geekbrains.home_work_6;
 public class Cat extends Animal {
 
     private static int countCat = 0;
+    public final int RUN_DISTANCE_CAT = 200;
 
     public Cat(String name, String color, int age){
         super(name, color, age);
@@ -11,9 +12,9 @@ public class Cat extends Animal {
 
     @Override
     public void run(int run){
-        if(run < 200) {
+        if(run < RUN_DISTANCE_CAT) {
             System.out.println(name + " пробежал " + run + " метров.");
-        } else System.out.println(name + " как и все коты может пробежать 200 метров, а он пробежал на " + (run - 200) + " метров больше и сдох((((" );
+        } else System.out.println(name + " как и все коты может пробежать " + RUN_DISTANCE_CAT + " метров, а он не может пробежал на " + (run - RUN_DISTANCE_CAT) + " метров больше." );
     }
 
     public void swim(int swim){
--- 1_Quater/Java_Level_1/Lesson_1/src/geekbrains/home_work_6/Dog.java
+++ 1_Quater/Java_Level_1/Lesson_1/src/geekbrains/home_work_6/Dog.java
@@ -3,6 +3,8 @@ package geekbrains.home_work_6;
 public class Dog extends Animal{
 
     private static int countDog = 0;
+    public final int RUN_DISTANCE_CAT = 500;
+    public final int SWIM_DISTANCE_CAT = 10;
 
     public Dog(String name, String color, int age){
         super(name, color, age);
@@ -11,15 +13,15 @@ public class Dog extends Animal{
 
     @Override
     public void run(int run){
-        if(run < 500) {
+        if(run < RUN_DISTANCE_CAT) {
             System.out.println(name + " пробежал " + run + " метров.");
-        } else System.out.println(name + " как и все собаки может пробежать 500 метров, а он пробежал на " + (run - 500) + " метров больше и сдох((((" );
+        } else System.out.println(name + " как и все собаки может пробежать " + RUN_DISTANCE_CAT + " метров, он не может пробежал на " + (run - RUN_DISTANCE_CAT) + " метров больше." );
     }
 
     public void swim(int swim){
-        if(swim < 10) {
+        if(swim < SWIM_DISTANCE_CAT) {
             System.out.println(name + " проплыл " + swim + " метров.");
-        } else System.out.println(name + " как и все собаки может проплыть 10 метров, а он проплыл на " + (swim - 10) + " метров больше и утонул((((" );
+        } else System.out.println(name + " как и все собаки может проплыть " + SWIM_DISTANCE_CAT + " метров, он не может проплыть на " + (swim - SWIM_DISTANCE_CAT) + " метров больше." );
     }
 
     public static void getCountDog(){
