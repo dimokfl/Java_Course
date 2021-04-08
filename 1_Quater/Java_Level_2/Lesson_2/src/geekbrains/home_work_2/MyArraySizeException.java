d01864b7a864db63902b539c0038b5634ce773d5
dimokfl <dimokfl@mail.ru>
2021-04-08 20:55:29 +0400

    Done HW2

---
+++ 1_Quater/Java_Level_2/Lesson_2/src/geekbrains/home_work_2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package geekbrains.home_work_2;
+
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException {
+    public MyArraySizeException (String massage){
+        super(massage);
+    }
+}
